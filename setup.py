6f0c7a90d3436286775056efb7b63e98cfa92852
Lowell Alleman <lowell87@gmail.com>
2021-04-03 22:22:05 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Lowell Alleman",
+    author_email='lowell87@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Splunk app boilerplate builder that uses a combination of Cookiecutter and Ksconf layers.",
+    entry_points={
+        'console_scripts': [
+            'crispy_cookie=crispy_cookie.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='crispy_cookie',
+    name='crispy_cookie',
+    packages=find_packages(include=['crispy_cookie', 'crispy_cookie.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/lowell80/crispy_cookie',
+    version='0.1.0',
+    zip_safe=False,
+)
