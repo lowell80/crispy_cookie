a9b97eef5898187289bd8eac48eeeb4d3f63e33a
Lowell Alleman <lowell87@gmail.com>
2021-04-20 18:00:04 -0400

    Bump version: 0.2.3 → 0.2.4

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/lowell80/crispy_cookie',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
